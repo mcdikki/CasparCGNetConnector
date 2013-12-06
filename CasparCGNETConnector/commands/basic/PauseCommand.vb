36a0da2d099d8db98042b50395237a98fb1fb449
Christopher Diekkamp <mcdikki@web.de>
2013-12-06 15:06:56 +0100

    Added *getter/setter for parameter to PauseCommand*

--- CasparCGNETConnector/commands/basic/PauseCommand.vb
+++ CasparCGNETConnector/commands/basic/PauseCommand.vb
@@ -28,7 +28,6 @@ Public Class PauseCommand
         Init(channel, layer)
     End Sub
 
-
     Private Sub Init(ByVal channel As Integer, ByVal layer As Integer)
         If channel > 0 Then DirectCast(getCommandParameter("channel"), CommandParameter(Of Integer)).setValue(channel)
         If layer > -1 Then DirectCast(getCommandParameter("layer"), CommandParameter(Of Integer)).setValue(layer)
@@ -44,6 +43,40 @@ Public Class PauseCommand
         Dim cmd As String = "PAUSE " & getDestination(getCommandParameter("channel"), getCommandParameter("layer"))
 
         Return escape(cmd)
+    End Function
+
+    Public Sub setChannel(ByVal channel As Integer)
+        If channel > 0 Then
+            DirectCast(getCommandParameter("channel"), CommandParameter(Of Integer)).setValue(channel)
+        Else
+            Throw New ArgumentException("Illegal argument channel=" + channel + ". The parameter channel has to be greater than 0.")
+        End If
+    End Sub
+
+    Public Function getChannel() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("channel")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setLayer(ByVal layer As Integer)
+        If layer < 0 Then
+            Throw New ArgumentException("Illegal argument layer=" + layer + ". The parameter layer has to be greater or equal than 0.")
+        Else
+            DirectCast(getCommandParameter("layer"), CommandParameter(Of Integer)).setValue(layer)
+        End If
+    End Sub
+
+    Public Function getLayer() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("layer")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
     End Function
 
     Public Overrides Function getRequiredVersion() As Integer()
