3f456008c05c4d1c662842a51cd7bd7dd4249536
Christopher Diekkamp <mcdikki@web.de>
2013-12-16 15:07:07 +0100

    Added *getter/setter to setCommand* Modified *setCommand to use getter/setter* Fixed *wrong parameter names in setCommand*

--- CasparCGNETConnector/commands/basic/SetCommand.vb
+++ CasparCGNETConnector/commands/basic/SetCommand.vb
@@ -37,10 +37,41 @@ Public Class SetCommand
     Public Overrides Function getCommandString() As String
         Dim cmd As String = "SET " & getDestination(getCommandParameter("channel"))
         If getCommandParameter("video mode").isSet Then
-            DirectCast(getCommandParameter("parameter"), CommandParameter(Of String())).getValue()
-            cmd = cmd & " " & DirectCast(getCommandParameter("video mode"), CommandParameter(Of String)).getValue()
+            cmd = cmd & " " & getVideomode()
         End If
         Return cmd
+    End Function
+
+    Public Sub setChannel(ByVal channel As Integer)
+        If channel > 0 Then
+            DirectCast(getCommandParameter("channel"), CommandParameter(Of Integer)).setValue(channel)
+        Else
+            Throw New ArgumentException("Illegal argument channel=" + channel + ". The parameter channel has to be greater than 0.")
+        End If
+    End Sub
+
+    Public Sub setVidemode(ByVal videomode As String)
+        If Not IsNothing(videomode) Then
+            DirectCast(getCommandParameter("video mode"), CommandParameter(Of String)).setValue(videomode)
+        End If
+    End Sub
+
+    Public Function getChannel() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("channel")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Function getVideomode() As String
+        Dim param As CommandParameter(Of String) = getCommandParameter("video mode")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
     End Function
 
     Public Overrides Function getRequiredVersion() As Integer()
