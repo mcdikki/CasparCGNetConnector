3c510ecb0b644f0a221003282f88d99d285f95d3
Christopher Diekkamp <mcdikki@web.de>
2013-12-16 16:07:41 +0100

    Added *getter/setter to stopCommand&swapCommand* Modified *StopCommand/SwapCommand to use getter/setter*

--- CasparCGNETConnector/commands/basic/StopCommand.vb
+++ CasparCGNETConnector/commands/basic/StopCommand.vb
@@ -46,6 +46,40 @@ Public Class StopCommand
         Return escape(cmd)
     End Function
 
+    Public Sub setChannel(ByVal channel As Integer)
+        If channel > 0 Then
+            DirectCast(getCommandParameter("channel"), CommandParameter(Of Integer)).setValue(channel)
+        Else
+            Throw New ArgumentException("Illegal argument channel=" + channel + ". The parameter channel has to be greater than 0.")
+        End If
+    End Sub
+
+    Public Function getChannel() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("channel")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setLayer(ByVal layer As Integer)
+        If layer < 0 Then
+            Throw New ArgumentException("Illegal argument layer=" + layer + ". The parameter layer has to be greater or equal than 0.")
+        Else
+            DirectCast(getCommandParameter("layer"), CommandParameter(Of Integer)).setValue(layer)
+        End If
+    End Sub
+
+    Public Function getLayer() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("layer")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
     Public Overrides Function getRequiredVersion() As Integer()
         Return {1}
     End Function
--- CasparCGNETConnector/commands/basic/SwapCommand.vb
+++ CasparCGNETConnector/commands/basic/SwapCommand.vb
@@ -18,12 +18,12 @@ Public Class SwapCommand
     Inherits AbstractCommand
 
     Public Sub New()
-        MyBase.New("SWAP", "Swapes the given channels or layers")
+        MyBase.New("SWAP", "Swaps the given channels or layers")
         InitParameter()
     End Sub
 
     Public Sub New(ByVal channelA As Integer, ByVal channelB As Integer, Optional ByVal layerA As Integer = -1, Optional ByVal layerB As Integer = -1)
-        MyBase.New("SWAP", "Swapss the given channels or layers")
+        MyBase.New("SWAP", "Swaps the given channels or layers")
         InitParameter()
         Init(channelA, channelB, layerA, layerB)
     End Sub
@@ -49,10 +49,78 @@ Public Class SwapCommand
         If getCommandParameter("layerA").isSet AndAlso getCommandParameter("layerB").isSet Then
             cmd = cmd & getDestination(getCommandParameter("channelA"), getCommandParameter("layerA")) & " " & getDestination(getCommandParameter("channelB"), getCommandParameter("layerB"))
         Else
-            cmd = cmd & DirectCast(getCommandParameter("channelA"), CommandParameter(Of Integer)).getValue & " " & DirectCast(getCommandParameter("channelB"), CommandParameter(Of Integer)).getValue
+            cmd = cmd & getChannelA() & " " & getChannelB()
         End If
 
         Return escape(cmd)
+    End Function
+
+    Public Sub setChannelA(ByVal channelA As Integer)
+        If channelA > 0 Then
+            DirectCast(getCommandParameter("channelA"), CommandParameter(Of Integer)).setValue(channelA)
+        Else
+            Throw New ArgumentException("Illegal argument channel=" + channelA + ". The parameter channel has to be greater than 0.")
+        End If
+    End Sub
+
+    Public Function getChannelA() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("channelA")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setLayerA(ByVal layerA As Integer)
+        If layerA < 0 Then
+            Throw New ArgumentException("Illegal argument layer=" + layerA + ". The parameter layer has to be greater or equal than 0.")
+        Else
+            DirectCast(getCommandParameter("layerA"), CommandParameter(Of Integer)).setValue(layerA)
+        End If
+    End Sub
+
+    Public Function getLayerA() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("layerA")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setChannelB(ByVal channelB As Integer)
+        If channelB > 0 Then
+            DirectCast(getCommandParameter("channelB"), CommandParameter(Of Integer)).setValue(channelB)
+        Else
+            Throw New ArgumentException("Illegal argument channel=" + channelB + ". The parameter channel has to be greater than 0.")
+        End If
+    End Sub
+
+    Public Function getChannelB() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("channelB")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setLayerB(ByVal layerB As Integer)
+        If layerB < 0 Then
+            Throw New ArgumentException("Illegal argument layer=" + layerB + ". The parameter layer has to be greater or equal than 0.")
+        Else
+            DirectCast(getCommandParameter("layerB"), CommandParameter(Of Integer)).setValue(layerB)
+        End If
+    End Sub
+
+    Public Function getLayerB() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("layerB")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
     End Function
 
     Public Overrides Function getRequiredVersion() As Integer()
