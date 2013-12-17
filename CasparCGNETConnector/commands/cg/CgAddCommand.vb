f1f05f2af3eaf51dc1b5257a69efaef1c4d3c90a
Christopher Diekkamp <mcdikki@web.de>
2013-12-17 02:19:20 +0100

    Added *Getter/Setter to CgAdd/CgClearCommand*

--- CasparCGNETConnector/commands/cg/CgAddCommand.vb
+++ CasparCGNETConnector/commands/cg/CgAddCommand.vb
@@ -56,19 +56,125 @@ Public Class CgAddCommand
     Public Overrides Function getCommandString() As String
         Dim cmd As String = "CG " & getDestination(getCommandParameter("channel"), getCommandParameter("layer")) & " ADD"
 
-        cmd = cmd & " '" & DirectCast(getCommandParameter("template"), CommandParameter(Of String)).getValue & "'"
-        cmd = cmd & " " & DirectCast(getCommandParameter("flashlayer"), CommandParameter(Of Integer)).getValue
+        cmd = cmd & " '" & getTemplate() & "'"
+        cmd = cmd & " " & getFlashlayer()
 
-        If getCommandParameter("play on load").isSet AndAlso DirectCast(getCommandParameter("play on load"), CommandParameter(Of Boolean)).getValue Then
+        If getPlayOnLoad() Then
             cmd = cmd & " 1"
         Else
             cmd = cmd & " 0"
         End If
         If getCommandParameter("data").isSet Then
-            cmd = cmd & " '" & DirectCast(getCommandParameter("data"), CommandParameter(Of String)).getValue & "'"
+            cmd = cmd & " '" & getData() & "'"
         End If
 
         Return escape(cmd)
+    End Function
+
+    Public Sub setChannel(ByVal channel As Integer)
+        If channel > 0 Then
+            DirectCast(getCommandParameter("channel"), CommandParameter(Of Integer)).setValue(channel)
+        Else
+            Throw New ArgumentException("Illegal argument channel=" + channel + ". The parameter channel has to be greater than 0.")
+        End If
+    End Sub
+
+    Public Function getChannel() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("channel")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setLayer(ByVal layer As Integer)
+        If layer < 0 Then
+            Throw New ArgumentException("Illegal argument layer=" + layer + ". The parameter layer has to be greater or equal than 0.")
+        Else
+            DirectCast(getCommandParameter("layer"), CommandParameter(Of Integer)).setValue(layer)
+        End If
+    End Sub
+
+    Public Function getLayer() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("layer")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setTemplate(ByVal template As String)
+        If IsNothing(template) Then
+            DirectCast(getCommandParameter("template"), CommandParameter(Of String)).setValue("")
+        Else
+            DirectCast(getCommandParameter("template"), CommandParameter(Of String)).setValue(template)
+        End If
+    End Sub
+
+    Public Sub setTemplate(ByVal template As CasparCGTemplate)
+        If IsNothing(template) Then
+            DirectCast(getCommandParameter("template"), CommandParameter(Of String)).setValue("")
+        Else
+            DirectCast(getCommandParameter("template"), CommandParameter(Of String)).setValue(template.getFullName)
+        End If
+    End Sub
+
+    Public Function getTemplate() As String
+        Dim param As CommandParameter(Of String) = getCommandParameter("template")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setFlashlayer(ByVal flashlayer As Integer)
+        If flashlayer < 0 Then
+            Throw New ArgumentException("Illegal argument flashlayer=" + flashlayer + ". The parameter flashlayer has to be greater or equal than 0.")
+        Else
+            DirectCast(getCommandParameter("flashlayer"), CommandParameter(Of Integer)).setValue(flashlayer)
+        End If
+    End Sub
+
+    Public Function getFlashlayer() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("flashlayer")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setPlayOnLoad(ByVal playOnLoad As Boolean)
+        DirectCast(getCommandParameter("play on load"), CommandParameter(Of Boolean)).setValue(playOnLoad)
+    End Sub
+
+    Public Function getPlayOnLoad() As Boolean
+        Dim param As CommandParameter(Of Boolean) = getCommandParameter("play on load")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setData(ByVal data As String)
+        If Not IsNothing(data) Then
+            DirectCast(getCommandParameter("data"), CommandParameter(Of String)).setValue(data)
+        Else
+            DirectCast(getCommandParameter("data"), CommandParameter(Of String)).setValue("")
+        End If
+    End Sub
+
+    Public Function getData() As String
+        Dim param As CommandParameter(Of String) = getCommandParameter("data")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
     End Function
 
     Public Overrides Function getRequiredVersion() As Integer()
--- CasparCGNETConnector/commands/cg/CgClearCommand.vb
+++ CasparCGNETConnector/commands/cg/CgClearCommand.vb
@@ -22,7 +22,7 @@ Public Class CgClearCommand
         InitParameter()
     End Sub
 
-    Public Sub New(ByVal channel As Integer, ByVal layer As Integer)
+    Public Sub New(ByVal channel As Integer, Optional ByVal layer As Integer = -1)
         MyBase.New("CG CLEAR", "Clears all layers and any state that might be stored")
         DirectCast(getCommandParameter("channel"), CommandParameter(Of Integer)).setValue(channel)
         If layer > -1 Then DirectCast(getCommandParameter("layer"), CommandParameter(Of Integer)).setValue(layer)
@@ -37,6 +37,40 @@ Public Class CgClearCommand
         Dim cmd As String = "CG " & getDestination(getCommandParameter("channel"), getCommandParameter("layer")) & " CLEAR"
 
         Return cmd
+    End Function
+
+    Public Sub setChannel(ByVal channel As Integer)
+        If channel > 0 Then
+            DirectCast(getCommandParameter("channel"), CommandParameter(Of Integer)).setValue(channel)
+        Else
+            Throw New ArgumentException("Illegal argument channel=" + channel + ". The parameter channel has to be greater than 0.")
+        End If
+    End Sub
+
+    Public Function getChannel() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("channel")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
+    End Function
+
+    Public Sub setLayer(ByVal layer As Integer)
+        If layer < 0 Then
+            Throw New ArgumentException("Illegal argument layer=" + layer + ". The parameter layer has to be greater or equal than 0.")
+        Else
+            DirectCast(getCommandParameter("layer"), CommandParameter(Of Integer)).setValue(layer)
+        End If
+    End Sub
+
+    Public Function getLayer() As Integer
+        Dim param As CommandParameter(Of Integer) = getCommandParameter("layer")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
     End Function
 
     Public Overrides Function getRequiredVersion() As Integer()
