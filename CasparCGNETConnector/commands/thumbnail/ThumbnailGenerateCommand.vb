631402681f060b5cf7409c7ba7cb607585aadad5
Christopher Diekkamp <mcdikki@web.de>
2014-03-23 13:47:39 +0100

    Modified *Added getter/setter to all thumbnail commands*

--- CasparCGNETConnector/commands/thumbnail/ThumbnailGenerateCommand.vb
+++ CasparCGNETConnector/commands/thumbnail/ThumbnailGenerateCommand.vb
@@ -24,13 +24,13 @@ Public Class ThumbnailGenerateCommand
     Public Sub New(ByVal media As String)
         MyBase.New("THUMBNAIL GENERATE", "Requests the server to (re-)generate the thumbnails for a specific media file")
         InitParameter()
-        DirectCast(getCommandParameter("media"), CommandParameter(Of String)).setValue(media)
+        setMedia(media)
     End Sub
 
     Public Sub New(ByVal media As AbstractCasparCGMedia)
         MyBase.New("THUMBNAIL GENERATE", "Requests the server to (re-)generate the thumbnails for a specific media file")
         InitParameter()
-        DirectCast(getCommandParameter("media"), CommandParameter(Of String)).setValue(media.getFullName)
+        setMedia(media)
     End Sub
 
     Private Sub InitParameter()
@@ -40,6 +40,31 @@ Public Class ThumbnailGenerateCommand
     Public Overrides Function getCommandString() As String
         Dim cmd = "THUMBNAIL GENERATE '" & DirectCast(getCommandParameter("media"), CommandParameter(Of String)).getValue & "'"
         Return escape(cmd)
+    End Function
+
+    Public Sub setMedia(ByVal media As String)
+        If IsNothing(media) Then
+            DirectCast(getCommandParameter("media"), CommandParameter(Of String)).setValue("")
+        Else
+            DirectCast(getCommandParameter("media"), CommandParameter(Of String)).setValue(media)
+        End If
+    End Sub
+
+    Public Sub setMedia(ByVal media As AbstractCasparCGMedia)
+        If IsNothing(media) Then
+            DirectCast(getCommandParameter("media"), CommandParameter(Of String)).setValue("")
+        Else
+            DirectCast(getCommandParameter("media"), CommandParameter(Of String)).setValue(media.getFullName)
+        End If
+    End Sub
+
+    Public Function getMedia() As String
+        Dim param As CommandParameter(Of String) = getCommandParameter("media")
+        If Not IsNothing(param) And param.isSet Then
+            Return param.getValue
+        Else
+            Return param.getDefault
+        End If
     End Function
 
     Public Overrides Function getRequiredVersion() As Integer()
